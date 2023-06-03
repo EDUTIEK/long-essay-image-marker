f4971d6d01d28dd8c1dca91ae9aa19cceef1dc6c
Fred Neumann <fred.neumann@fau.de>
2023-06-04 00:47:08 +0200

    added playground

--- Mark.js
+++ Mark.js
@@ -3,87 +3,126 @@
  */
 class Mark {
 
-    static SHAPE_POINT = 'point';
+    static SHAPE_CIRCLE = 'circle';
     static SHAPE_RECTANGLE = 'rectangle';
     static SHAPE_POLYGON = 'polygon';
 
-
     /**
-     * Unique identifier of the mark
-     * Should be auto-generated for a new mark
+     * Unique identifier of the mark, should be auto-generated for a new mark.
+     * Maximum length is 50 characters.
      * @type {string}
      */
     key = '';
 
     /**
-     * Label that is shown when a mark is selected
+     * Label that is shown when a mark is selected.
+     * Maximum length is 10 characters.
      * @type {string}
      */
     label = '';
 
     /**
-     * Indicates whether it is an own mark or the mark of another corrector
-     * @type {boolean}
-     */
-    is_own = true;
-
-    /**
-     * Indicates whether it marks an excellent passage
-     * @type {boolean}
-     */
-    is_excellent = false;
-
-    /**
-     * Indicates whether it marks a cardinal failure
-     * @type {boolean}
-     */
-    is_cardinal = false;
-
-    /**
-     * Indicates whether the mark should be highlighted as selected
-     * @type {boolean}
-     */
-    is_selected = false;
-
-    /**
-     * Ty
+     * Color of the mark
      * @type {string}
      */
-    shape = Mark.SHAPE_POINT;
+    color: '#D8E5F4';
+
 
     /**
-     * X Position of the mark
+     * Shape of the mark
      * @type {string}
      */
-    x_position = '';
+    shape = Mark.SHAPE_CIRCLE;
 
     /**
-     * Y Position of the mark
+     * Horizontal position of the mark in pixels on the full-width page image
      * @type {string}
      */
-    y_position = '';
-
+    x_position = 0;
 
     /**
-     * Width of a mark (rectangle)
+     * Vertical porizontal position of the mark in pixels on the full-width page image
+     * @type {string}
+     */
+    y_position = 0;
+
+    /**
+     * Width of a rectangle
      * @type {integer}
      */
     width = 0;
 
     /**
-     * Height of the mark (rectangle)
+     * Height of a rectangle
      * @type {integer}
      */
     height = 0;
 
-
     /**
-     * Path of positions of a polygon
-     * Each array element os a pair ox x and y position
-     * Example: [ [3,5], [4,6], [5,7], ... ]
+     * Path of the following positions of a polygon
+     * Each array element is a pair of offsets from the x_position and y_position
+     * Example: [ [5,-1], [6,-2], [7,0], ... ]
      * @type {array}
      */
     poly_path = [];
+
+
+    /**
+     * Constructor - get properties from a data object
+     * @param {object} data
+     */
+    constructor(data) {
+        if (data.key !== undefined && data.key !== null) {
+            this.key = data.key.toString()
+        } else if (this.key == '') {
+            // get a random key
+            this.key = 'mark' + Math.random().toString();
+        }
+        if (data.label !== undefined && data.label !== null) {
+            this.label = data.label.toString()
+        }
+        if (data.color !== undefined && data.color !== null) {
+            this.color = data.color.toString()
+        }
+        if ([Mark.SHAPE_CIRCLE, Mark.SHAPE_RECTANGLE, Mark.SHAPE_POLYGON].includes(data.shape)) {
+            this.shape = data.shape;
+        }
+        if (data.x_position !== undefined && data.x_position !== null) {
+            this.x_position = parseInt(data.x_position);
+        }
+        if (data.y_position !== undefined && data.y_position !== null) {
+            this.y_position = parseInt(data.y_position);
+        }
+        if (data.width !== undefined && data.width !== null) {
+            this.width = parseInt(data.width);
+        }
+        if (data.height !== undefined && data.height !== null) {
+            this.height = parseInt(data.height);
+        }
+        if (Array.isArray(data.poly_path)) {
+            data.poly_path.forEach(elements => {
+                if (Array.isArray(element) && element.length = 2) {
+                    this.poly_path.push(parseInt(element[0], parseInt(element[1])));
+                }
+            });
+        }
+    }
+
+    /**
+     * Get a plain data object from the properties
+     * @return {object}
+     */
+    getData() {
+        return {
+            key: this.key,
+            label: this.label,
+            color: this.color,
+            shape: this.shape,
+            x_position: this.x_position,
+            y_position: this.y_position,
+            poly_path: this.poly_path
+        }
+    }
 }
 
 export default Mark;
